3ae05ce8333d65ec92ba2b41f54ee0c25fccbcef
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2023-12-01 14:53:15 -0700

    button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert('hello world');
+const yearEl = document.getElementById('year');
+const date = new Date().getFullYear();
+console.log(date);
+yearEl.textContent = date;
+
+// Access the button element
+const button = document.getElementById('myButton');
+// Add an event listener to change the button's text when clicked
+button.addEventListener('click', function() {
+button.textContent = 'Button Clicked';
+});
