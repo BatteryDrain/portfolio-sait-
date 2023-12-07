31637b8e3456701d8d47e42d5545a158b97da88e
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2023-12-06 20:23:08 -0700

    java

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+alert("HELLOWORLD!");
 const yearEl = document.getElementById('year');
 const date = new Date().getFullYear();
 console.log(date);
 yearEl.textContent = date;
 
 // Access the button element
-let indicator = 1
+let indicator = 0
 const button = document.getElementById('myButton');
-button.textContent = indicator
+button.textContent = "click me!"
+//mouse over
+button.addEventListener('mouseover', function(){
+    document.getElementById('myButton').innerText = "clicked "+indicator+" times";
+});
 // Add an event listener to change the button's text when clicked
 button.addEventListener('click', function() {
-    button.textContent = indicator++
+    button.textContent = "clicked "+indicator+++" times";
 });
+//mouse leave
+button.addEventListener('mouseleave', function(){
+    document.getElementById('myButton').innerText = "click me!";
+});
+//calculate
+let mod = "even"
+if (indicator % 2 == 1){
+    mod = "odd";
+};
+document.getElementById("mod").innerText = mod;
